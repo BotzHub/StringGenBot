c4ce809bce71b0778afeff730fde7d262f132ee4
Botz Hub <botzhub01@gmail.com>
2025-05-19 23:43:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-LOG_GROUP_ID = -1002468318697  # Replace with your actual log group ID
+LOG_GROUP = -1002468318697  # Replace with your actual log group ID
 
 OWNER_ID = int(getenv("OWNER_ID", 7142782067))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MrGhostsx2")
